c3f883a1331d35c89b7a3ff78fd261f6d13260fb
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 21:38:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(num)
   if num < 0 || num == 0 || num == 1
     return false
   else
-    (2..num - 1).to_a.all? do |n|
+    (2.. num - 1).to_a.all? do |n|
       num % n != 0
     end
   end
