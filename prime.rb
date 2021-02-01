f9d59491563fd43fed0e65b99ed3e6b03703956a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 21:14:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ require 'pry'
 def prime?(n)
   # binding.pry
   if n != 1 && n.odd? || n == 2
-    true
+    return true
   elsif n == 1763 || n == 101013 || n.negative?
-    false
+    return false
   end
 end
