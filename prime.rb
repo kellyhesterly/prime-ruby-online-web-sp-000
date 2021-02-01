a03d079b322daf17c904b1db2fe00279188222be
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 21:30:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,10 @@ require 'pry'
 def prime?(num)
   num = (2..)
   num.each do |n|
-    if num % n == 0 || n.negative?
+    if num % n == 0
       return false
+    elsif !(n.negative?)
+      return true
     end
     return true
   end
